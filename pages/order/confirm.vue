1bd538f8e58f38e8f728167c94273d0b2444a51a
itmonkey <itmonkeyws@163.com>
2020-07-27 18:28:18 +0800

    add:支付方式

--- pages/order/confirm.vue
+++ pages/order/confirm.vue
@@ -93,6 +93,123 @@
 		<sh-picker-modal v-if="pickerData.couponList" @changeCoupon="changeCoupon" v-model="showPicker" :pickerData="pickerData"></sh-picker-modal>
 		<!-- 登录提示 -->
 		<shopro-login-modal></shopro-login-modal>
+		<!-- 配送方式弹窗 -->
+		<shopro-modal v-model="showExpressType" :modalType="'bottom-modal'">
+			<block slot="modalContent">
+				<!-- 配送方式 -->
+				<view class="express-type">
+					<view class="express-type__head">
+						<view class="express-type__head-nav" v-for="(nav, index) in expressType" :key="nav.id" @tap="changeExpressType(index)">
+							<text class="head-nav__title" :class="{ 'head-nav__title--active': expressTypeCur === index }">{{ nav.title }}</text>
+							<view :class="expressClass" v-show="expressTypeCur === index"></view>
+						</view>
+					</view>
+					<view class="express-type__content">
+						<view class="empty-address" v-if="!addressId" @tap="jump('/pages/user/address/list', { from: 'order' })">
+							请添加收货地址
+							<text class="cuIcon-right"></text>
+						</view>
+						<!-- 快递 -->
+						<view class="express-address" v-if="addressId && expressTypeCur == 0">
+							<view class="express-top" @tap="jump('/pages/user/address/list', { from: 'order' })">
+								<text class="tag" v-if="address.is_default == 1">默认</text>
+								<text class="address">{{ address.province_name }}{{ address.city_name }}{{ address.area_name }}{{ address.address }}</text>
+								<text class="cuIcon-right address-guide"></text>
+								<view class="address-location">
+									<image class="location-img" src="/static/imgs/order/e0.png" mode=""></image>
+									<text class="location-text">物流快递</text>
+								</view>
+							</view>
+							<view class="express-content">
+								<view class="phone-box1">
+									<text class="name">{{ address.consignee }}</text>
+									<text class="phone">{{ address.phone }}</text>
+								</view>
+							</view>
+							<view class="express-bottom"></view>
+						</view>
+						<!-- 自提 -->
+						<view class="express-address" v-if="addressId && expressTypeCur == 1">
+							<view class="express-top" @tap="jump('/pages/order/business-address', { from: 'order' })">
+								<text class="tag1" v-if="address.is_default == 1">最近</text>
+								<text class="address">{{ address.province_name }}{{ address.city_name }}{{ address.area_name }}{{ address.address }}</text>
+								<text class="cuIcon-right address-guide"></text>
+								<view class="address-location">
+									<image class="location-img" src="/static/imgs/order/e1.png" mode=""></image>
+									<text class="location-text">距您650m</text>
+								</view>
+							</view>
+							<view class="express-content">
+								<view class="time-box">
+									<text class="box-title">配送时间</text>
+									<view class="box-content" @tap="checkExpressTime('shop')">
+										<text class="box-text">{{ checkTime['time'][checkTimeCur] }}</text>
+										<text class="cuIcon-right box-icon"></text>
+									</view>
+								</view>
+								<view class="box-line"></view>
+								<view class="phone-box">
+									<text class="box-title">预留电话</text>
+									<view class="box-content x-f">
+										<input class="edit-phone" type="number" v-model="selfPhone" />
+										<text class="cuIcon-write box-icon"></text>
+									</view>
+								</view>
+							</view>
+							<view class="express-bottom">
+								<label class="x-f" @tap="checkProtocol">
+									<checkbox class="round protocol-checkbox orange" :class="{ checked: isProtocol }" :checked="true"></checkbox>
+									<view class="protocol">
+										同意并接受
+										<text class="protocol-text" @tap="jump('/pages/public/richtext', { id: 3 })">《到店自提服务协议》</text>
+									</view>
+								</label>
+							</view>
+						</view>
+						<!-- 商家 -->
+						<view class="express-address" v-if="addressId && expressTypeCur == 2">
+							<view class="express-top" @tap="jump('/pages/user/address/list', { from: 'order' })">
+								<text class="tag" v-if="address.is_default == 1">默认</text>
+								<text class="address">{{ address.province_name }}{{ address.city_name }}{{ address.area_name }}{{ address.address }}</text>
+								<text class="cuIcon-right address-guide"></text>
+								<view class="address-location">
+									<image class="location-img" src="/static/imgs/order/e2.png" mode=""></image>
+									<text class="location-text">商家配送</text>
+								</view>
+							</view>
+							<view class="express-content">
+								<view class="time-box">
+									<text class="box-title">配送时间</text>
+									<view class="box-content" @tap="checkExpressTime('shop')">
+										<text class="box-text">{{ checkTime['time'][checkTimeCur] }}</text>
+										<text class="cuIcon-right box-icon"></text>
+									</view>
+								</view>
+								<view class="box-line"></view>
+								<view class="phone-box">
+									<text class="box-title">预留电话</text>
+									<view class="box-content x-f">
+										<input class="edit-phone" type="number" v-model="selfPhone" />
+										<text class="cuIcon-write box-icon"></text>
+									</view>
+								</view>
+							</view>
+							<view class="express-bottom"></view>
+						</view>
+						<!-- 自动 -->
+						<view class="express-address" v-if="addressId && expressTypeCur == 3">
+							<view class="express-top">
+								<text class="dispatch-notice">订单支付完成后，请在订单详情页查看发货信息</text>
+								<view class="address-location">
+									<image class="location-img" src="/static/imgs/order/e3.png" mode=""></image>
+									<text class="location-text">自动发货</text>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</block>
+		</shopro-modal>
 	</view>
 </template>
 
@@ -125,10 +242,54 @@ export default {
 			remark: '',
 			orderPre: {},
 			couponId: 0,
-			couponPrice: '选择优惠券'
+			couponPrice: '选择优惠券',
+			showExpressType: true ,//配送方式弹窗
+			expressTypeCur: 0,
+			expressType: [
+				//快递方式
+				{
+					id: 'e1',
+					title: '物流快递'
+				},
+				{
+					id: 'e2',
+					title: '上门自提'
+				},
+				{
+					id: 'e3',
+					title: '商家配送'
+				},
+				{
+					id: 'e4',
+					title: '自动发货'
+				}
+			],
+			isProtocol: true, //自提协议。
+			selfPhone: 15625892568, //编辑手机号
+			checkType: '自提',
+			checkTime: {
+				//选择配送时间数据
+				day: ['今天（周五）', '明天（周六）'],
+				time: ['15:30', '15:50', '16:10', '16:30', '16:50', ' 17:10']
+			},
+			checkTimeCur: 0, //默认选中。
+			checkTimeId: 'c1',
+			checkDayCur: 0
 		};
 	},
-	computed: {},
+	computed: {
+		expressClass() {
+			let cl = 'head-nav--active';
+			const { expressType, expressTypeCur } = this;
+			if (expressTypeCur === 0) {
+				cl = 'head-nav__left--active';
+			}
+			if (expressTypeCur === expressType.length - 1) {
+				cl = 'head-nav__right--active';
+			}
+			return cl;
+		}
+	},
 	watch: {
 		address(val, oldVal) {
 			if (this.address) {
@@ -427,4 +588,264 @@ export default {
 		color: #fff;
 	}
 }
+// 弹窗之配送方式
+// 配送方式
+.express-type {
+	width: 750rpx;
+	background-color: #fff;
+	border-radius: 20rpx 20rpx 0 0;
+	min-height: 700rpx;
+	.express-type__head {
+		width: 100%;
+		height: 62rpx;
+		background: #faf0dc;
+		@include flex($align: center);
+		border-radius: 20rpx 20rpx 0 0;
+		&-nav {
+			flex: 1;
+			@include flex($align: center, $justify: center);
+			position: relative;
+			height: 100%;
+		}
+		.head-nav--active {
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			bottom: 0;
+			background: #fff;
+			width: 100%;
+			height: 74rpx;
+			background-color: #fff;
+			border-radius: 20rpx 20rpx 0px 0px;
+			&::after {
+				content: '';
+				display: block;
+				width: 40rpx;
+				height: 74rpx;
+				position: absolute;
+				transform: skewX(20deg);
+				background: #fff;
+				border-top-right-radius: 20rpx;
+				top: 0;
+				right: -15rpx;
+			}
+			&::before {
+				content: '';
+				display: block;
+				width: 40rpx;
+				height: 74rpx;
+				position: absolute;
+				transform: skewX(-20deg);
+				background: #fff;
+				border-top-left-radius: 20rpx;
+				top: 0;
+				left: -15rpx;
+			}
+		}
+		.head-nav__left--active {
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			bottom: 0;
+			background: #fff;
+			width: 100%;
+			height: 74rpx;
+			background-color: #fff;
+			border-radius: 20rpx 20rpx 0px 0px;
+			&::after {
+				content: '';
+				display: block;
+				width: 40rpx;
+				height: 74rpx;
+				position: absolute;
+				transform: skewX(20deg);
+				background: #fff;
+				border-top-right-radius: 20rpx;
+				top: 0;
+				right: -15rpx;
+			}
+		}
+		.head-nav__right--active {
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			bottom: 0;
+			background: #fff;
+			width: 100%;
+			height: 74rpx;
+			background-color: #fff;
+			border-radius: 20rpx 20rpx 0px 0px;
+			&::before {
+				content: '';
+				display: block;
+				width: 40rpx;
+				height: 74rpx;
+				position: absolute;
+				transform: skewX(-20deg);
+				background: #fff;
+				border-top-left-radius: 20rpx;
+				top: 0;
+				left: -15rpx;
+			}
+		}
+		.head-nav__title {
+			font-size: 24rpx;
+			font-weight: 500;
+			color: #666;
+			position: relative;
+			z-index: 6;
+		}
+		.head-nav__title--active {
+			color: #a8700d;
+			font-size: 26rpx;
+		}
+	}
+	.express-type__content {
+		.empty-address {
+			height: 120rpx;
+			padding: 0 25rpx;
+			@include flex($justify: null, $align: center, $direction: null, $warp: null, $warpAlign: null);
+			font-size: 28rpx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: rgba(153, 153, 153, 1);
+		}
+		// 快递
+		.express-address {
+			position: relative;
+			padding: 30rpx 25rpx;
+			background: url('~@/static/imgs/order/address_bg.png') no-repeat;
+			background-size: 430rpx 300rpx;
+			background-position: top right;
+			.express-top {
+				padding-bottom: 20rpx;
+				width: 450rpx;
+				.address {
+					font-size: 28rpx;
+					font-family: PingFang SC;
+					font-weight: 500;
+					color: rgba(51, 51, 51, 1);
+					line-height: 40rpx;
+				}
+				.dispatch-notice {
+					font-size: 28rpx;
+					font-family: PingFang SC;
+					font-weight: 500;
+					color: rgba(51, 51, 51, 1);
+					line-height: 40rpx;
+				}
+				.address-location {
+					@include flex($justify: center, $align: center, $direction: column, $warp: null, $warpAlign: null);
+					position: absolute;
+					right: 60rpx;
+					top: 30rpx;
+					.location-img {
+						width: 80rpx;
+						height: 90rpx;
+					}
+					.location-text {
+						font-size: 18rpx;
+						font-family: PingFang SC;
+						font-weight: 500;
+						color: rgba(51, 51, 51, 1);
+					}
+				}
+				.tag {
+					background: rgba(233, 191, 113, 0.2);
+					border-radius: 6rpx;
+					padding: 0 16rpx;
+					line-height: 38rpx;
+					color: #a8700d;
+					font-size: 22rpx;
+					margin-right: 20rpx;
+				}
+				.tag1 {
+					background: rgba(53, 190, 105, 0.2);
+					border-radius: 6rpx;
+					padding: 0 16rpx;
+					line-height: 38rpx;
+					color: #1bbc50;
+					font-size: 22rpx;
+					margin-right: 20rpx;
+				}
+				.address-guide {
+					position: absolute;
+					right: 25rpx;
+					top: 40rpx;
+					color: #999999;
+				}
+			}
+
+			.express-content {
+				margin: 20rpx 0;
+				@include flex($justify: null, $align: center, $direction: null, $warp: null, $warpAlign: null);
+				.box-line {
+					width: 1rpx;
+					height: 61rpx;
+					border-left: 1rpx solid rgba(238, 238, 238, 1);
+					margin: 0 40rpx;
+				}
+				.phone-box1 {
+					.name,
+					.phone {
+						font-size: 26rpx;
+						font-family: PingFang SC;
+						font-weight: 400;
+						color: rgba(102, 102, 102, 1);
+					}
+
+					.phone {
+						margin-left: 20rpx;
+					}
+				}
+				.time-box,
+				.phone-box {
+					.box-title {
+						font-size: 24rpx;
+						font-family: PingFang SC;
+						font-weight: 400;
+						color: #666;
+						padding-bottom: 10rpx;
+					}
+					.box-text {
+						font-size: 24rpx;
+						font-family: PingFang SC;
+						font-weight: 500;
+						color: #333;
+					}
+					.edit-phone {
+						width: 160rpx;
+						font-size: 24rpx;
+						font-family: PingFang SC;
+						font-weight: 500;
+						color: #333;
+					}
+					.box-icon {
+						font-size: 28rpx;
+						color: #999;
+						display: inline-block;
+						width: 40rpx;
+						text-align: center;
+						line-height: 40rpx;
+					}
+				}
+			}
+			.express-bottom {
+				.protocol-checkbox {
+					transform: scale(0.7);
+				}
+				.protocol {
+					font-size: 24rpx;
+					font-family: PingFang SC;
+					font-weight: 400;
+					color: rgba(102, 102, 102, 1);
+					.protocol-text {
+						color: #6487a4;
+					}
+				}
+			}
+		}
+	}
+}
+
 </style>
