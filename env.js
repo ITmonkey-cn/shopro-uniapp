e652e1773cb224fa90ef6ee5fed36bce08e784a4
itmonkey <itmonkeyws@163.com>
2020-09-29 14:38:28 +0800

    merge:dev

--- env.js
+++ env.js
@@ -15,4 +15,4 @@ const ENV_BASE_URL = {
 
 export const BASE_URL = ENV_BASE_URL[process.env.NODE_ENV || 'development']; //后台根域名
 export const API_URL = ENV_API_URL[process.env.NODE_ENV || 'development']; //后台接口域名
-export const HAS_LIVE = true; //后台是否开通直播权限,根据情况在manifest.json中，开启注释相应组件的引入。
+export const HAS_LIVE = false; //后台是否开通直播权限,根据情况在manifest.json中，开启注释相应组件的引入。
