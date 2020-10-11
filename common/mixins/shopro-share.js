301195cbfe23c3b671e36f20154ea16559e8c0f7
lidongtony <lidongtony@gmail.com>
2020-10-11 16:46:48 +0800

    疑似因为url后面多了一个/ 导致小程序分享不出去

--- common/mixins/shopro-share.js
+++ common/mixins/shopro-share.js
@@ -155,7 +155,7 @@ export default {
 				if (url.indexOf('?') !== -1) {
 					url = `${url}&${queryArr.join('&')}`
 				} else {
-					url = `${url}/?${queryArr.join('&')}`
+					url = `${url}?${queryArr.join('&')}`
 				}
 			}
 			return url;
