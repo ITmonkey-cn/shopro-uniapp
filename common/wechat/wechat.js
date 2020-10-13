6cc79ea22e17d05934a1eadced59dea7350e0f1c
itmonkey <itmonkeyws@163.com>
2020-10-13 15:30:31 +0800

    edit:小程序自动更新

--- common/wechat/wechat.js
+++ common/wechat/wechat.js
@@ -154,28 +154,43 @@ export default class Wechat {
 		});
 	}
 
+	// 小程序更新
 	checkMiniProgramUpdate() {
-		let updateManager = uni.getUpdateManager();
-		updateManager.onCheckForUpdate(function(res) {
-			// 请求完新版本信息的回调
-		});
-		updateManager.onUpdateReady(function(res) {
-			uni.showModal({
-				title: '更新提示',
-				content: '新版本已经准备好，是否重启应用？',
-				success(res) {
-					if (res.confirm) {
-						// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-						updateManager.applyUpdate();
-					}
+		if (uni.canIUse('getUpdateManager')) {
+			const updateManager = uni.getUpdateManager()
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				if (res.hasUpdate) {
+					updateManager.onUpdateReady(function() {
+						uni.showModal({
+							title: '更新提示',
+							content: '新版本已经准备好，是否重启应用？',
+							success: function(res) {
+								console.log('success====', res)
+								// res: {errMsg: "showModal: ok", cancel: false, confirm: true}
+								if (res.confirm) {
+									// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+									updateManager.applyUpdate()
+								}
+							}
+						})
+					})
+					updateManager.onUpdateFailed(function() {
+						// 新的版本下载失败
+						uni.showModal({
+							title: '已经有新版本了哟~',
+							content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+						})
+					})
 				}
-			});
-		});
-		updateManager.onUpdateFailed(function(res) {
-			// 新的版本下载失败
-		});
+			})
+		} else {
+			uni.showModal({
+				title: '提示',
+				content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+			})
+		}
 	}
-
 	// #endif
 
 
